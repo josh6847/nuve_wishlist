131a0896d58f19f4add8a5249490419d0ccc05c8
josh <joshrachner@josh-rachners-macbook-pro.local>
2009-12-30 19:36:51 -0600

    email

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   
   def account_verification(recipient)
     recipients     recipient['email']
-    from           "donotreply@lanuve.com"
+    from           "Nuve Admin <donotreply@lanuve.com>"
     subject        "Welcome to The Butler by LaNuve"
     body           :recipient => recipient
     content_type   "text/html"
